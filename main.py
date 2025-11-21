35096cdb1aa32e89004af007756796854a5d0c06
ChetanDhillon07 <99910244+ChetanDhillon07@users.noreply.github.com>
2025-11-21 14:45:53 +0530

    Change Flask app to run on host and port from env

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask,request,render_template,jsonify
 import pickle
 import pandas as pd
+import os
 
 app=Flask(__name__)
 
@@ -42,4 +43,4 @@ def home():
 
     
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port = int(os.environ.get("PORT", 8080)))
